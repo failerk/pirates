cfcbd4c52c1d708eebcb31f21f64d08844357b7f
Kyle Pfahler <pfahlerk1@gmail.com>
2020-01-05 12:27:55 -0500

    Added 2dcamera to ship

--- src/Autoload/PlayerData.gd
+++ src/Autoload/PlayerData.gd
@@ -4,6 +4,9 @@ export (int) var health: = 3 setget set_health
 
 signal health_updated
 
-func set_health(value):
+func set_health(value) -> void:
 	health = value
 	emit_signal("health_updated")
+
+func reset_state() -> void:
+	health = 3
